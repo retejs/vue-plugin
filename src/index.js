8854ff4247e19cb67ee04a9f7ff32dc501ee4b09
Ni55aN <huccah@gmail.com>
2019-09-26 19:40:15 +0300

    v0.5.0, pass options to Vue instances

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import Socket from './Socket.vue';
 import Vue from 'vue';
 import mixin from './mixin';
 
-function createVue(el, vueComponent, vueProps) {
+function createVue(el, vueComponent, vueProps, options = {}) {
     const app = new Vue({
-        render: h => h(vueComponent, { props: vueProps })
+        render: h => h(vueComponent, { props: vueProps }),
+        ...options
     });
 
     const nodeEl = document.createElement('div');
@@ -17,20 +18,20 @@ function createVue(el, vueComponent, vueProps) {
     return app;
 }
 
-function createNode(editor, CommonVueComponent, { el, node, component, bindSocket, bindControl }) {
+function createNode(editor, CommonVueComponent, { el, node, component, bindSocket, bindControl }, options) {
     const vueComponent = component.component || CommonVueComponent || Node;
     const vueProps = { ...component.props, node, editor, bindSocket, bindControl };
-    const app = createVue(el, vueComponent, vueProps);
+    const app = createVue(el, vueComponent, vueProps, options);
 
     node.vueContext = app.$children[0];
 
     return app;
 }
 
-function createControl(editor, { el, control }) {
+function createControl(editor, { el, control }, options) {
     const vueComponent = control.component;
     const vueProps = { ...control.props, getData: control.getData.bind(control), putData: control.putData.bind(control) };
-    const app = createVue(el, vueComponent, vueProps);
+    const app = createVue(el, vueComponent, vueProps, options);
 
     control.vueContext = app.$children[0];
 
@@ -46,16 +47,16 @@ const update = (entity) => {
     });
 }
 
-function install(editor, { component: CommonVueComponent }) {
+function install(editor, { component: CommonVueComponent, options }) {
     editor.on('rendernode', ({ el, node, component, bindSocket, bindControl }) => {
         if (component.render && component.render !== 'vue') return;
-        node._vue = createNode(editor, CommonVueComponent, { el, node, component, bindSocket, bindControl });
+        node._vue = createNode(editor, CommonVueComponent, { el, node, component, bindSocket, bindControl }, options);
         node.update = async () => await update(node);
     });
 
     editor.on('rendercontrol', ({ el, control }) => {
         if (control.render && control.render !== 'vue') return;
-        control._vue = createControl(editor, { el, control });
+        control._vue = createControl(editor, { el, control }, options);
         control.update = async () => await update(control)
     });
 
